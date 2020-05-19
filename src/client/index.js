11b0b92524ec542d8da40a3081b95b7e2958300e
akuto aoyama <aaaa.akuto@gmail.com>
2020-05-19 19:46:19 +0900

    index.html修正し、表示成功

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
 fetch('/api/').then(response => {
   console.log(response.json());
