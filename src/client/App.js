6d7af4c5fc343572032a7005e474a7880891f517
akuto aoyama <aaaa.akuto@gmail.com>
2020-05-20 01:23:37 +0900

    App.js修正

--- src/client/App.js
+++ src/client/App.js
@@ -1,9 +1,25 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+// import './App.scss'
+import { Button } from '@zeit-ui/react'
+
+// import { ZeitProvider, CssBaseline } from '@zeit-ui/react'
+
+// const Application = () => (
+//   <ZeitProvider>
+//     <CssBaseline />
+//     <App />
+//   </ZeitProvider>
+// )
 
 interface Props {
   text: string,
 }
+
+function zeitBtn() {
+  return <Button>Click Me</Button>;
+}
+
 
 function Text(props: Props) {
   return <div style={{ color: 'green' }}>{props.text}</div>;
@@ -18,6 +34,7 @@ export default function App() {
         <Text text={text} />
         入力して下さい
       </h1>
+      <zeitBtn />
       <input
         type="text"
         value={text}
@@ -36,3 +53,8 @@ export const Index = () => {
   return <div>name</div>;
 };
 ReactDOM.render(<Index />, document.getElementById('index'));
+
+// import { Button } from '@zeit-ui/react'
+
+// export const MyComponent = () => <Button>Click Me</Button>
+// ReactDOM.render(<MyComponent />, document.getElementById('button'));
